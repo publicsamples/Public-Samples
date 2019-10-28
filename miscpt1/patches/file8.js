d60525704aec35f7bd61490b01485071387ab03b
modularsamples <modularsamples@gmail.com>
2019-10-28 01:25:42 +0000

    patches

---
+++ miscpt1/patches/file8.js
@@ -0,0 +1,18 @@
+var keys = new Tone.Sampler({
+		
+			"C1" : "file1.[mp3|ogg]",
+			"E1" : "file5.[mp3|ogg]",
+			"G#1" : "file9.[mp3|ogg]",
+			"C2" : "file13.[mp3|ogg]",
+			"E2" : "file17.[mp3|ogg]",
+			"G#2" : "file21.[mp3|ogg]",
+			"C3" : "file25.[mp3|ogg]"
+			
+				
+					}, {
+
+						"baseUrl" : "https://raw.githubusercontent.com/publicsamples/Misc-Synth-Samples/master/mp3/JP4K3Combo/"
+					})					.connect(filter);
+					
+				
+
---
+++ miscpt2/patches/file1.js
@@ -0,0 +1,18 @@
+var keys = new Tone.Sampler({
+		
+			"C1" : "file1.[mp3|ogg]",
+			"E1" : "file5.[mp3|ogg]",
+			"G#1" : "file9.[mp3|ogg]",
+			"C2" : "file13.[mp3|ogg]",
+			"E2" : "file17.[mp3|ogg]",
+			"G#2" : "file21.[mp3|ogg]",
+			"C3" : "file25.[mp3|ogg]"
+			
+				
+					}, {
+
+						"baseUrl" : "https://github.com/publicsamples/Misc-Synth-Samples-pt2/blob/master/mp3/1st/"
+					})					.connect(filter);
+					
+				
+
