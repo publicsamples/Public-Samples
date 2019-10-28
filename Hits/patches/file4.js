74f769d85bdc29b63035d7f0e2062128703b007f
modularsamples <modularsamples@gmail.com>
2019-10-28 04:02:36 +0000

    Hits

---
+++ Hits/patches/file4.js
@@ -0,0 +1,26 @@
+var keys = new Tone.Players({
+		
+			"1" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file1.[mp3|ogg]",
+			"2" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file2.[mp3|ogg]",
+			"3" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file3.[mp3|ogg]",
+			"4" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file4.[mp3|ogg]",
+			"5" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file5.[mp3|ogg]",
+			"6" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file6.[mp3|ogg]",
+			"7" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file7.[mp3|ogg]",
+			"8" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file8.[mp3|ogg]",
+			"9" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file9.[mp3|ogg]",
+			"10" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file10.[mp3|ogg]",
+			"11" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file11.[mp3|ogg]",
+			"12" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file12.[mp3|ogg]",
+			"13" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file13.[mp3|ogg]",
+			"14" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file14.[mp3|ogg]",
+			"15" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file15.[mp3|ogg]",
+			"16" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file16.[mp3|ogg]",
+			"17" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file17.[mp3|ogg]",
+			"18" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file18.[mp3|ogg]",
+			"19" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file19.[mp3|ogg]",
+			"20" : "https://github.com/publicsamples/hits/raw/master/mp3/Roland%20SH-2%20Kit/file20.[mp3|ogg]"
+		})					.connect(filter);
+					
+				
+
