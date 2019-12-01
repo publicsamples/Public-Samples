7029d79f7112013197d8592cefa2edc8e724eee3
Richard Taylor <rick@hankgeorge.com>
2019-12-01 01:56:48 +0000

    jp presets

--- presets/jp4/file11.js
+++ presets/jp4/file11.js
@@ -1,12 +1,12 @@
 
 var keys = new Tone.Sampler({
-		"C1" : "file1.[mp3|ogg]",
-			"E1" : "file5.[mp3|ogg]",
-			"G#1" : "file9.[mp3|ogg]",
-			"C2" : "file13.[mp3|ogg]",
-			"E2" : "file17.[mp3|ogg]",
-			"G#2" : "file21.[mp3|ogg]",
-			"C3" : "file25.[mp3|ogg]"
+		"C1" : "File1.[mp3|ogg]",
+			"E1" : "File5.[mp3|ogg]",
+			"G#1" : "File9.[mp3|ogg]",
+			"C2" : "File13.[mp3|ogg]",
+			"E2" : "File17.[mp3|ogg]",
+			"G#2" : "File21.[mp3|ogg]",
+			"C3" : "File25.[mp3|ogg]"
 				
 				
 				
