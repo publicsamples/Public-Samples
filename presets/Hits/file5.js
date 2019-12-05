0780f63f2fe10a9ef272bfd21c6af457820d10cc
Richard Taylor <rick@hankgeorge.com>
2019-12-05 17:17:09 +0000

    fix missing pages

---
+++ presets/Hits/file5.js
@@ -0,0 +1,26 @@
+var keys = new Tone.Players({
+		
+			"1" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file1.[mp3|ogg]",
+			"2" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file2.[mp3|ogg]",
+			"3" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file3.[mp3|ogg]",
+			"4" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file4.[mp3|ogg]",
+			"5" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file5.[mp3|ogg]",
+			"6" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file6.[mp3|ogg]",
+			"7" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file7.[mp3|ogg]",
+			"8" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file8.[mp3|ogg]",
+			"9" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file9.[mp3|ogg]",
+			"10" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file10.[mp3|ogg]",
+			"11" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file11.[mp3|ogg]",
+			"12" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file12.[mp3|ogg]",
+			"13" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file13.[mp3|ogg]",
+			"14" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file14.[mp3|ogg]",
+			"15" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file15.[mp3|ogg]",
+			"16" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file16.[mp3|ogg]",
+			"17" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file17.[mp3|ogg]",
+			"18" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file18.[mp3|ogg]",
+			"19" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file19.[mp3|ogg]",
+			"20" : "https://raw.githubusercontent.com/publicsamples/hits/master/mp3/Xpander%20Kits/file20.[mp3|ogg]"
+		})					.connect(filter);
+					
+				
+
