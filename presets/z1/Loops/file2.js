0006950b86b60f867268810ca651575bab13ba5a
Rick <53375860+modularsamples@users.noreply.github.com>
2020-01-22 23:45:32 +0000

    more stuff

---
+++ presets/z1/Loops/file2.js
@@ -0,0 +1,9 @@
+var keys = new Tone.GrainPlayer({
+			"url" : "/presets/z1/Loops/Biscuits.mp3"
+	
+		}).connect(filter);
+					
+		document.querySelector("tone-oscilloscope").bind(keys);
+		document.querySelector("tone-grain-player").bind(keys);
+document.querySelector("tone-play-toggle").bind(keys);
+
