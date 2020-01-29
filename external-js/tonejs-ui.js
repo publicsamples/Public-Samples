aba8c8d4e9da1012f761da8643f3175b8ffb1fd7
Rick <53375860+modularsamples@users.noreply.github.com>
2020-01-29 16:39:16 +0000

    morph

--- external-js/tonejs-ui.js
+++ external-js/tonejs-ui.js
@@ -1399,7 +1399,7 @@ function g(t){return"slot"===t.localName}class y{static getFlattenedNodes(t){ret
 				:host {
 					display: inline-block;
 					width: 100%;
-					height: 375px;
+					height: 300px;
 				}
 				#container {
 					width: 100%;
