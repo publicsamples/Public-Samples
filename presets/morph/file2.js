43d1eec6742ccf05640f4153e29076284073aa45
Rick <53375860+modularsamples@users.noreply.github.com>
2020-01-29 16:38:01 +0000

    morph

---
+++ presets/morph/file2.js
@@ -0,0 +1,44 @@
+var keys = new Tone.Sampler({
+			"C0" : "file1.[mp3|ogg]",
+	        "C#0" : "file2.[mp3|ogg]",
+	        "D0" : "file3.[mp3|ogg]",
+			"D#0" : "file4.[mp3|ogg]",
+			"E0" : "file5.[mp3|ogg]",
+			"F0" : "file6.[mp3|ogg]",
+			"F#0" : "file7.[mp3|ogg]",
+			"G0" : "file8.[mp3|ogg]",
+			"G#0" : "file9.[mp3|ogg]",
+			"A0" : "file10.[mp3|ogg]",
+			"A#0" : "file11.[mp3|ogg]",
+			"B0" : "file12.[mp3|ogg]",
+			"C1" : "file13.[mp3|ogg]",
+	        "C#1" : "file14.[mp3|ogg]",
+	        "D1" : "file15.[mp3|ogg]",
+			"D#1" : "file16.[mp3|ogg]",
+			"E1" : "file17.[mp3|ogg]",
+			"F1" : "file18.[mp3|ogg]",
+			"F#1" : "file19.[mp3|ogg]",
+			"G1" : "file20.[mp3|ogg]",
+			"G#1" : "file21.[mp3|ogg]",
+			"A1" : "file22.[mp3|ogg]",
+			"A#1" : "file23.[mp3|ogg]",
+			"B1" : "file24.[mp3|ogg]",
+			"C2" : "file25.[mp3|ogg]",
+	        "C#2" : "file26.[mp3|ogg]",
+	        "D2" : "file27.[mp3|ogg]",
+			"D#2" : "file28.[mp3|ogg]",
+			"E2" : "file29.[mp3|ogg]",
+			"F2" : "file30.[mp3|ogg]",
+			"F#2" : "file31.[mp3|ogg]",
+			"G2" : "file32.[mp3|ogg]",
+			"G#2" : "file33.[mp3|ogg]",
+			"A2" : "file34.[mp3|ogg]",
+			"A#2" : "file35.[mp3|ogg]",
+			"B2" : "file36.[mp3|ogg]",
+			"C3" : "file37.[mp3|ogg]"
+				
+					}, {
+
+						"baseUrl" : "/presets/morph/disturber/"
+					})					.connect(filter);
+					
